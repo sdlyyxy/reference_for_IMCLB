33af5616602ac1b2348fd07c6c6e291e20230688
sdlyyxy <sdlyyxy@icloud.com>
2017-07-20 16:59:50 +0800

    Add content in Readme.md

---
+++ make_toc.py
@@ -0,0 +1,14 @@
+import os
+_root = os.getcwd()
+for root, dirs, files in os.walk(_root):
+    if root != _root:
+        break
+    for name in dirs:
+        if name == '.git' or name == '.vscode':
+            continue
+        print "* %s" % name
+        for _root_, _dirs, mdfiles in os.walk(os.path.join(root, name)):
+            for mdfile in mdfiles:
+                if mdfile == '.DS_Store':
+                    continue
+                print "\t* [%s](%s)" % (mdfile[:-3], os.path.join(_root_, mdfile).replace(_root,''))
